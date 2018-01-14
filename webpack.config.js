c83adb22fdb7585df02da9efbf430f63172f0b6a
Bernardo Cotrim <bmmcotrim@gmail.com>
2018-01-14 11:14:52 +0000

    Webpack config and module dist fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const libraryName = 'js-roman-numerals';
+const outputFile = 'js-roman-numerals.js';
+
 module.exports = {
   entry: './src/romanNumeral.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({ minimize: true }),
@@ -21,6 +27,11 @@ module.exports = {
             presets: ['babel-preset-env'],
           },
         },
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
       },
     ],
   },
